d82debf0d3c464398e24bac1f05abc0dbbfafb75
vagabond <vagabond.git@gmail.com>
2026-09-10 21:32:39 +0200

    chore: update dsv lib version

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,4 @@
-import { CdkDragEnd, DragDropModule } from '@angular/cdk/drag-drop';
+import { DragDropModule } from '@angular/cdk/drag-drop';
 import { Component, effect, inject, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { BaseMainContainer } from '@ng-vagabond-lab/ng-dsv/base';
@@ -43,47 +43,11 @@ export class AppComponent extends BaseMainContainer {
 
     readonly menu = signal<MenuDto>(menu);
 
-    isOpen = true;
-
-    menuPosition = { x: 0, y: 0 }; // Déclenché quand le pouce lâche l'écran
-
-    onDragEnded(event: CdkDragEnd) {
-        const distanceX = event.distance.x;
-        console.log(distanceX);
-
-        // Si on a glissé vers la gauche de plus de 80px -> Fermer
-        if (distanceX < -80) {
-            this.closeMenu();
-        } else {
-            // Sinon, on remet le menu en position ouverte
-            this.openMenu();
-        }
-    }
-
-    closeMenu() {
-        this.isOpen = false;
-        // -280px correspond à la largeur exacte de ton menu en CSS
-        this.menuPosition = { x: -280, y: 0 };
-    }
-
-    openMenu() {
-        this.isOpen = true;
-        this.menuPosition = { x: 0, y: 0 };
-    }
-
-    private resizeListener?: () => void;
-
     constructor() {
         super();
         effect(() => {
-            if (this.environmentService.platformService.isPlatformBrowser() && window.visualViewport) {
-                this.resizeListener = () => {
-                    const vh = window.visualViewport?.height;
-                    document.documentElement.style.setProperty('--app-height', `${vh}px`);
-                };
-
-                window.visualViewport.addEventListener('resize', this.resizeListener);
-                this.resizeListener();
+            if (this.authService.userConnected()) {
+                this.notificationPushService.registerAndGetToken();
             }
         });
         effect(() => {
@@ -91,15 +55,5 @@ export class AppComponent extends BaseMainContainer {
                 this.notificationService.fetchNbRead();
             }
         });
-        effect(() => {
-            if (this.authService.userConnected()) {
-                this.notificationPushService.registerAndGetToken();
-            }
-        });
-    }
-    ngOnDestroy() {
-        if (this.resizeListener && window.visualViewport) {
-            window.visualViewport.removeEventListener('resize', this.resizeListener);
-        }
     }
 }
